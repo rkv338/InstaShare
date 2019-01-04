ea6196bd21a6f89d4e0fc2a3427261fedad789ea
Unknown <vikturi99@gmail.com>
2019-01-04 13:05:04 -0600

    Get link from js file to html popup

--- instashare.js
+++ instashare.js
@@ -1,12 +1,14 @@
 var tab_id;
 browser.browserAction.onClicked.addListener((tab) => {
 	tab_id = tab.url;
+	document.getElementById("qrcode").src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + tab_url;
 	title();
 });
 
 function title () {
 	browser.browserAction.setTitle({
-		title: 'QR Code',
+		title: 'QR Code 1',
 		tabId: tab_id 
 	});
 }
+
